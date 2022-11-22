eda2fd8c4a3be3d631804533b3227e156f1fde04
John Rey <adioskid@mail.ru>
2022-11-22 21:58:20 +0300

    Homework#9

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    String name;
+    String surname;
+
+    Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getSurname() {
+        return surname;
+    }
+}
--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,28 @@
 public class Book {
+    String titleBook;
+    Author author;
+    int publicYear;
+
+    Book(String titleBook, Author author, int publicYear) {
+        this.titleBook = titleBook;
+        this.author = author;
+        this.publicYear = publicYear;
+    }
+
+    String getTitleBook() {
+        return titleBook;
+    }
+
+    Author getAuthor() {
+        return author;
+    }
+
+    int getPublicYear() {
+        return publicYear;
+    }
+
+    void setPublicYear(int year) {
+        publicYear = year;
+    }
+
 }
